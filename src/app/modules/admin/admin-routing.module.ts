c9ee46a8ca4d5528c2254251bcf09a96dfe5c0ad
wuyeelok <wuyeelok@gmail.com>
2024-05-02 19:00:27 +0800

    Copy: Admin header component

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'about', component: AboutComponent },
-      { path: 'service', component: ServicesComponent },
+      { path: 'services', component: ServicesComponent },
       { path: 'contact', component: ContactComponent },
       { path: '', redirectTo: 'admin/home', pathMatch: 'full' },
     ],
