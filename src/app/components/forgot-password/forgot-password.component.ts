ebd0336787e8805ca203d833ab5e03b3463148c8
wuyeelok <wuyeelok@gmail.com>
2024-05-02 16:50:05 +0800

    Copy: Login and forgot page

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  { faLock } from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  faLock = faLock;
 
   constructor() { }
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  { faLock } from "@fortawesome/free-solid-svg-icons"
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  faLock = faLock;
 
   constructor() { }
 
